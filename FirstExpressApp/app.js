2db80f76b3ac68b460366ab59223755bfcdb18b5
Akansha Agarwal <akurox123@gmail.com>
2020-07-26 11:51:07 +0530

    add files

--- FirstExpressApp/app.js
+++ FirstExpressApp/app.js
@@ -1,2 +1,34 @@
+// http://localhost:3000
 var express = require("express");
 var app = express();
+
+// "/"->hi there
+app.get("/", function(req, res) {
+	res.send("HI!There");
+});
+// "/bye"->Goodbye
+app.get("/bye", function(req, res) {
+	res.send("GoodBye");
+});
+// "/dog"->Goodbye
+app.get("/dog", function(req, res) {
+	res.send("MEEEEEEEOOOOWW!!!");
+});
+
+//for a pattern:Route parameters
+app.get("/r/:name", function(req, res) {
+	var x = req.params.name;
+	res.send("welcomeeee to " + x.toUpperCase() + " subreddit page!");
+});
+app.get("/r/:name/comments/:id/:title", function(req, res) {
+	res.send("welcomeeee to commments pageeeee!!!");
+});
+
+// "for any other route"
+app.get("*", function(req, res) {
+	res.send("You are a star!!");
+});
+
+app.listen(3000, function() {
+	console.log("Example app listening on port 3000!");
+});
