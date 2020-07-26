4045eb55531e2f18b90791b34cc6b3c68a1a4c87
Akansha Agarwal <akurox123@gmail.com>
2020-07-26 12:08:44 +0530

    add ejs files

--- MoreExpress/EJSDemo/app.js
+++ MoreExpress/EJSDemo/app.js
@@ -1,4 +1,10 @@
 var express = require("express");
 var app = express();
 
-app.listen();
+app.get("/", function(req, res) {
+	res.render("home.ejs");
+});
+
+app.listen(3000, function() {
+	console.log("Example app listening on port 3000!");
+});
